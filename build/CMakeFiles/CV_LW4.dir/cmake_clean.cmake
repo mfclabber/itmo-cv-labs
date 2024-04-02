397986fb08a737b7b0180df1116636c16c7ffb3b
mfclabber <novichkovdmitriy04@gmail.com>
2024-04-02 13:13:55 +0300

    added build dir

---
+++ build/CMakeFiles/CV_LW4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CV_LW4.dir/src/lab4.cpp.o"
+  "CV_LW4"
+  "CV_LW4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CV_LW4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CV_LW4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CV_LW4.
+# This may be replaced when dependencies are built.
