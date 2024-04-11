67d9ef78f459b681cc6023b3077635151dafe563
mfclabber <novichkovdmitriy04@gmail.com>
2024-04-11 14:43:03 +0300

    partially fulfilled

--- lab5/src/lab5.cpp
+++ lab5/src/lab5.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 #include <opencv2/opencv.hpp> 
 
+
+
 struct func{
     func(std::vector<cv::Vec4i>& lines, cv::Mat& image): lines{lines}, image{image} {};
     std::vector<cv::Vec4i> lines;
     cv::Mat image;
+    static bool comp(cv::Vec4i& , cv::Vec4i&);
+    static uint lenght_line(cv::Vec4i&);
 };
+
 
 func canny_hough_tranform(cv::Mat& image){
 
@@ -37,59 +42,179 @@ func canny_hough_tranform(cv::Mat& image){
 }
 
 
+func ordinary_hough_tranform(cv::Mat& image){
+
+    cv::Mat image_new, image_out = image.clone();
+    cv::cvtColor(image, image_new, cv::COLOR_BGR2GRAY);
+
+    cv::threshold(image_new, image_new, 50, 255, cv::THRESH_BINARY);
+
+    std::vector<cv::Vec4i> linesP;
+
+    HoughLinesP(image_new, linesP, 1, CV_PI / 180, 50, 50, 15);
+
+    for (int i = 0; i < linesP.size(); ++i){
+        cv::Vec4i I = linesP[i];
+        cv::line(image_out, cv::Point(I[0], I[1]),
+                            cv::Point(I[2], I[3]),
+                            cv::Scalar(0, 255, 0), 
+                                  1, cv::LINE_AA);
+
+        cv::circle(image_out, cv::Point(I[0], I[1]), 1,
+                                       (127, 0, 60), 5, 
+                                          cv::LINE_AA);
+
+        cv::circle(image_out, cv::Point(I[2], I[3]), 1, 
+                                        (0, 0, 255), 5, 
+                                          cv::LINE_AA);
+    }
+    
+    return func(linesP, image_out);
+}
+
+bool func::comp(cv::Vec4i& l1, cv::Vec4i& l2){
+    return (std::sqrt(std::pow(abs(l1[2] - l1[0]), 2) + std::pow(abs(l1[3] - l1[1]), 2)) < 
+            std::sqrt(std::pow(abs(l2[2] - l2[0]), 2) + std::pow(abs(l2[3] - l2[1]), 2)));
+}
+
+uint func::lenght_line(cv::Vec4i& l){
+    return std::sqrt(std::pow(abs(l[2] - l[0]), 2) + 
+                     std::pow(abs(l[3] - l[1]), 2));
+}
+
 int main(int argc, char** argv) 
 { 
     std::string path = "/home/mfclabber/itmo-cv-labs/lab5"; 
 
-    cv::Mat image, image_new;
-    image = cv::imread(path + "/source/3.png", 1);
-    // cv::Canny(image, image_new, 50, 200);
+
+    // HOUGH TRANSFORM FOR LINES
+    // IMAGE 1
+
+    // GRAYSCALE
+
+    // cv::Mat image;
+    // image = cv::imread(path + "/source/1.png", 1);
+
+    // func output = ordinary_hough_tranform(image);
+
+    // std::cout << "MAX LENGTH = " << func::lenght_line(*std::max_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nMIN LENGTH = " << func::lenght_line(*std::min_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nNUMBER OF LINES = " << output.lines.size();
 
 
-    // Image 1. Grayscale
+    // cv::imwrite(path + "/outputs/image1_ordinary.png", output.image);
+    // cv::imshow("image", output.image);
+    // cv::waitKey();
 
-    // cv::Mat image_out = image.clone();
+    // MAX LENGTH = 787
+    // MIN LENGTH = 51
+    // NUMBER OF LINES = 73
 
-    // std::vector<cv::Vec2f> lines;
-    // cv::HoughLines(image_new, lines, 1, CV_PI / 180, 150);
+    // CANNY
 
-    // for( size_t i = 0; i < lines.size(); i++ )
-    // {
-    //     float rho = lines[i][0], theta = lines[i][1];
-    //     cv::Point pt1, pt2;
-    //     double a = cos(theta), b = sin(theta);
-    //     double x0 = a*rho, y0 = b*rho;
-    //     pt1.x = cvRound(x0 + 1000*(-b));
-    //     pt1.y = cvRound(y0 + 1000*(a));
-    //     pt2.x = cvRound(x0 - 1000*(-b));
-    //     pt2.y = cvRound(y0 - 1000*(a));
-    //     cv::line(image_out, pt1, pt2, cv::Scalar(0,0,255), 1, cv::LINE_AA);
-    // }
+    // cv::Mat image;
+    // image = cv::imread(path + "/source/1.png", 1);
 
+    // func output = canny_hough_tranform(image);
 
-    // std::vector<cv::Vec4i> linesP;
-    // HoughLinesP(image_new, linesP, 1, CV_PI / 180, 50, 50, 15);
-
-    // for (int i = 0; i < linesP.size(); ++i){
-    //     cv::Vec4i I = linesP[i];
-    //     cv::line(image_out, cv::Point(I[0], I[1]),
-    //                         cv::Point(I[2], I[3]),
-    //                         cv::Scalar(0, 255, 0), 
-    //                               1, cv::LINE_AA);
-
-    //     cv::circle(image_out, cv::Point(I[0], I[1]), 1,
-    //                                    (127, 0, 60), 5, 
-    //                                       cv::LINE_AA);
-
-    //     cv::circle(image_out, cv::Point(I[2], I[3]), 1, 
-    //                                     (0, 0, 255), 5, 
-    //                                       cv::LINE_AA);
-    // }
-
-    func output = canny_hough_tranform(image);
+    // std::cout << "MAX LENGTH = " << func::lenght_line(*std::max_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nMIN LENGTH = " << func::lenght_line(*std::min_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nNUMBER OF LINES = " << output.lines.size();
 
 
-    // cv::imwrite(path + "/outputs/image_binarization.png", image_new);
-    cv::imshow("image", output.image);
-    cv::waitKey();
+    // cv::imwrite(path + "/outputs/image1_canny.png", output.image);
+    // cv::imshow("image", output.image);
+    // cv::waitKey();
+
+    // MAX LENGTH = 427
+    // MIN LENGTH = 50
+    // NUMBER OF LINES = 183
+
+
+
+    // IMAGE 2
+
+    // GRAYSCALE
+
+    // cv::Mat image;
+    // image = cv::imread(path + "/source/2.png", 1);
+
+    // func output = ordinary_hough_tranform(image);
+
+    // std::cout << "MAX LENGTH = " << func::lenght_line(*std::max_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nMIN LENGTH = " << func::lenght_line(*std::min_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nNUMBER OF LINES = " << output.lines.size();
+
+
+    // cv::imwrite(path + "/outputs/image2_ordinary.png", output.image);
+    // cv::imshow("image", output.image);
+    // cv::waitKey();
+
+    // MAX LENGTH = 1290
+    // MIN LENGTH = 50
+    // NUMBER OF LINES = 984
+
+    // CANNY
+
+    // cv::Mat image;
+    // image = cv::imread(path + "/source/2.png", 1);
+
+    // func output = canny_hough_tranform(image);
+
+    // std::cout << "MAX LENGTH = " << func::lenght_line(*std::max_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nMIN LENGTH = " << func::lenght_line(*std::min_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nNUMBER OF LINES = " << output.lines.size();
+
+
+    // cv::imwrite(path + "/outputs/image2_canny.png", output.image);
+    // cv::imshow("image", output.image);
+    // cv::waitKey();
+
+    // MAX LENGTH = 539
+    // MIN LENGTH = 50
+    // NUMBER OF LINES = 44
+
+
+
+    // IMAGE 3
+
+    // GRAYSCALE
+
+    // cv::Mat image;
+    // image = cv::imread(path + "/source/4.png", 1);
+
+    // func output = ordinary_hough_tranform(image);
+
+    // std::cout << "MAX LENGTH = " << func::lenght_line(*std::max_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nMIN LENGTH = " << func::lenght_line(*std::min_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nNUMBER OF LINES = " << output.lines.size();
+
+
+    // cv::imwrite(path + "/outputs/image3_ordinary.png", output.image);
+    // cv::imshow("image", output.image);
+    // cv::waitKey();
+
+    // MAX LENGTH = 735
+    // MIN LENGTH = 53
+    // NUMBER OF LINES = 738
+
+    // CANNY
+
+    // cv::Mat image;
+    // image = cv::imread(path + "/source/4.png", 1);
+
+    // func output = canny_hough_tranform(image);
+
+    // std::cout << "MAX LENGTH = " << func::lenght_line(*std::max_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nMIN LENGTH = " << func::lenght_line(*std::min_element(output.lines.begin(), output.lines.end(), func::comp)) <<
+    //              "\nNUMBER OF LINES = " << output.lines.size();
+
+
+    // cv::imwrite(path + "/outputs/image3_canny.png", output.image);
+    // cv::imshow("image", output.image);
+    // cv::waitKey();
+
+    // MAX LENGTH = 469
+    // MIN LENGTH = 52
+    // NUMBER OF LINES = 52
 }
