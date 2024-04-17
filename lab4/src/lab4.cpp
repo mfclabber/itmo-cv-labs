1f9d52c1fe00d33f19eb9005bd1a1c10738405be
mfclabber <novichkovdmitriy04@gmail.com>
2024-04-17 14:48:31 +0300

    update media

--- lab4/src/lab4.cpp
+++ lab4/src/lab4.cpp
@@ -171,25 +171,25 @@ int main(int argc, char** argv)
 
     // Segmentation Veber
     
-    // image = cv::imread(path + "/source/face.png", 0);
-    // image_new = image.clone();
+    image = cv::imread(path + "/source/face.png", 0);
+    image_new = image.clone();
 
-    // for(int i = 0; i < image_new.rows; ++i){
-    //     for(int j = 0; j < image_new.cols; ++j){
-    //         if(image_new.at<uchar>(i, j) <= 88)
-    //             image_new.at<uchar>(i, j) = 20 - 12 * image_new.at<uchar>(i, j) / 88;   
+    for(int i = 0; i < image_new.rows; ++i){
+        for(int j = 0; j < image_new.cols; ++j){
+            if(image_new.at<uchar>(i, j) <= 110)
+                image_new.at<uchar>(i, j) = 50 - 12 * image_new.at<uchar>(i, j) / 88;   
             
-    //         else if(image_new.at<uchar>(i, j) <= 138)
-    //             image_new.at<uchar>(i, j) = 0.002 * cv::pow(image_new.at<uchar>(i, j) - 88, 2); 
+            else if(image_new.at<uchar>(i, j) <= 150)
+                image_new.at<uchar>(i, j) = 0.0002 * cv::pow(image_new.at<uchar>(i, j) - 88, 2); 
             
-    //         else if(image_new.at<uchar>(i, j) <= 255)
-    //             image_new.at<uchar>(i, j) = 7 * (image_new.at<uchar>(i, j) - 138) / 117; 
-    //     }
-    // }
+            else if(image_new.at<uchar>(i, j) <= 255)
+                image_new.at<uchar>(i, j) = 0.07 * (image_new.at<uchar>(i, j) - 138) / 117; 
+        }
+    }
 
-    // cv::imwrite(path + "/outputs/image_veber_segmentation.png", image_new);
-    // cv::imshow("image", image_new);
-    // cv::waitKey();
+    cv::imwrite(path + "/outputs/image_veber_segmentation.png", image_new);
+    cv::imshow("image", image_new);
+    cv::waitKey();
 
 
 
@@ -233,86 +233,86 @@ int main(int argc, char** argv)
     // Texture segmentation
 
 
-    image = cv::imread(path + "/source/sea.png", 0);
+    // image = cv::imread(path + "/source/sea.png", 0);
 
-    cv::Mat E, Eim;
-    cv::Mat el = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
-    entropy(image, E, el);
+    // cv::Mat E, Eim;
+    // cv::Mat el = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
+    // entropy(image, E, el);
 
-    double Emin, Emax;
-    cv::minMaxLoc(E, &Emin, &Emax);
-    Eim = (E - Emin) / (Emax - Emin);
-    Eim.convertTo(Eim, CV_8U, 255);
-    cv::Mat BW1;
-    cv::threshold(Eim, BW1, 0, 255, cv::THRESH_OTSU);
+    // double Emin, Emax;
+    // cv::minMaxLoc(E, &Emin, &Emax);
+    // Eim = (E - Emin) / (Emax - Emin);
+    // Eim.convertTo(Eim, CV_8U, 255);
+    // cv::Mat BW1;
+    // cv::threshold(Eim, BW1, 0, 255, cv::THRESH_OTSU);
 
-    cv::imwrite(path + "/outputs/image_entropy_segmentation.png", BW1);
+    // cv::imwrite(path + "/outputs/image_entropy_segmentation.png", BW1);
     
-    cv::Mat BWao, closeBWao, Mask1;
-    bwareaopen(BW1, BWao, 50000);
-    cv::imwrite(path + "/outputs/image_bwareoopen_segmentation.png", BWao);
+    // cv::Mat BWao, closeBWao, Mask1;
+    // bwareaopen(BW1, BWao, 50000);
+    // cv::imwrite(path + "/outputs/image_bwareoopen_segmentation.png", BWao);
 
-    cv::Mat nhood = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
-    cv::morphologyEx(BWao, closeBWao, cv::MORPH_CLOSE, nhood);
+    // cv::Mat nhood = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
+    // cv::morphologyEx(BWao, closeBWao, cv::MORPH_CLOSE, nhood);
 
-    imfillholes(closeBWao, Mask1);
-    cv::imwrite(path + "/outputs/image_imclose_segmentation.png", Mask1);
+    // imfillholes(closeBWao, Mask1);
+    // cv::imwrite(path + "/outputs/image_imclose_segmentation.png", Mask1);
 
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(Mask1, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
-    cv::Mat boundary = cv::Mat::zeros(Mask1.rows, Mask1.cols, CV_8UC1);
+    // std::vector<std::vector<cv::Point>> contours;
+    // cv::findContours(Mask1, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+    // cv::Mat boundary = cv::Mat::zeros(Mask1.rows, Mask1.cols, CV_8UC1);
     
-    cv::drawContours(boundary, contours, -11, 255, 1);
-    cv::imwrite(path + "/outputs/image_countours_segmentation.png", boundary);
+    // cv::drawContours(boundary, contours, -11, 255, 1);
+    // cv::imwrite(path + "/outputs/image_countours_segmentation.png", boundary);
 
-    cv::Mat segmentResults = image.clone();
-    segmentResults.setTo(cv::Scalar(255), boundary != 0);
-    cv::imwrite(path + "/outputs/image_segmentResults_segmentation.png", segmentResults);
+    // cv::Mat segmentResults = image.clone();
+    // segmentResults.setTo(cv::Scalar(255), boundary != 0);
+    // cv::imwrite(path + "/outputs/image_segmentResults_segmentation.png", segmentResults);
 
-    cv::Mat I2 = image.clone();
-    I2.setTo(0, Mask1 != 0);
+    // cv::Mat I2 = image.clone();
+    // I2.setTo(0, Mask1 != 0);
 
-    cv::imwrite(path + "/outputs/I.png",I2);
-    cv::imshow("image", I2);
+    // cv::imwrite(path + "/outputs/I.png",I2);
+    // cv::imshow("image", I2);
 
-    cv::Mat E2, Eim2;
+    // cv::Mat E2, Eim2;
 
-    entropy(I2, E2, el);;
+    // entropy(I2, E2, el);;
 
-    double Emin2, Emax2;
-    cv::minMaxLoc(E2, &Emin2, &Emax2);
+    // double Emin2, Emax2;
+    // cv::minMaxLoc(E2, &Emin2, &Emax2);
 
-    Eim2 = (E2 - Emin2) /  (Emax2 -  Emin2);
-    Eim2.convertTo(Eim2, CV_8U, 255);
+    // Eim2 = (E2 - Emin2) /  (Emax2 -  Emin2);
+    // Eim2.convertTo(Eim2, CV_8U, 255);
     
-    cv::Mat BW2;
+    // cv::Mat BW2;
 
-    cv::threshold(Eim2, BW2, 0, 255, cv::THRESH_OTSU);
-    cv::imwrite(path + "/outputs/image_entropy2_segmentation.png", BW2);
+    // cv::threshold(Eim2, BW2, 0, 255, cv::THRESH_OTSU);
+    // cv::imwrite(path + "/outputs/image_entropy2_segmentation.png", BW2);
 
-    cv::Mat BW2ao, closeBW2ao, Mask2;
-    bwareaopen(BW2, BW2ao, 50000);
-    cv::imwrite(path + "/outputs/image_bwareoopen2_segmentation.png", BW2ao);
+    // cv::Mat BW2ao, closeBW2ao, Mask2;
+    // bwareaopen(BW2, BW2ao, 50000);
+    // cv::imwrite(path + "/outputs/image_bwareoopen2_segmentation.png", BW2ao);
 
-    cv::morphologyEx(BW2ao, closeBW2ao, cv::MORPH_CLOSE, nhood);
-    imfillholes(closeBW2ao, Mask2);
-    cv::imwrite(path + "/outputs/image_imclose2_segmentation.png", Mask2);
+    // cv::morphologyEx(BW2ao, closeBW2ao, cv::MORPH_CLOSE, nhood);
+    // imfillholes(closeBW2ao, Mask2);
+    // cv::imwrite(path + "/outputs/image_imclose2_segmentation.png", Mask2);
 
-    std::vector<std::vector<cv::Point>> contours2;
-    cv::findContours(Mask2, contours2, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
-    cv::Mat boundary2 = cv::Mat::zeros(Mask2.rows, Mask2.cols, CV_8UC1);
-    cv::drawContours(boundary2, contours2, -1, 255, 1);
-    cv::imwrite(path + "/outputs/image_countours2_segmentation.png", boundary2);
+    // std::vector<std::vector<cv::Point>> contours2;
+    // cv::findContours(Mask2, contours2, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+    // cv::Mat boundary2 = cv::Mat::zeros(Mask2.rows, Mask2.cols, CV_8UC1);
+    // cv::drawContours(boundary2, contours2, -1, 255, 1);
+    // cv::imwrite(path + "/outputs/image_countours2_segmentation.png", boundary2);
 
-    cv::Mat segmentResults2 = I2.clone();
-    segmentResults2.setTo(255, boundary2 != 0);
-    cv::imwrite(path + "/outputs/image_segmentResults2_segmentation.png", segmentResults2);
+    // cv::Mat segmentResults2 = I2.clone();
+    // segmentResults2.setTo(255, boundary2 != 0);
+    // cv::imwrite(path + "/outputs/image_segmentResults2_segmentation.png", segmentResults2);
 
-    I2 = image.clone();
-    I2.setTo(0, Mask2 != 0);
+    // I2 = image.clone();
+    // I2.setTo(0, Mask2 != 0);
 
-    cv::imwrite(path + "/outputs/I2.png",I2);
-    cv::imshow("image", I2);
+    // cv::imwrite(path + "/outputs/I2.png",I2);
+    // cv::imshow("image", I2);
 
-    cv::waitKey();
+    // cv::waitKey();
 }
